5b1f756df0def8b71ac4f073e5b893a2c87be704
amittal2001 <73400261+amittal2001@users.noreply.github.com>
2025-08-24 11:46:38 +0300

    Update run_azure.py

--- scripts/run_azure.py
+++ scripts/run_azure.py
@@ -127,7 +127,7 @@ def launch_vm_and_job(  worker_id,
     except:
         # start the compute instance, if it doesn't exist
         logging.info(f"Creating compute instance {compute_instance_name}...")
-        idle_time_before_shutdown_minutes=600
+        idle_time_before_shutdown_minutes=60
 
         if use_managed_identity:
             identity_config = ManagedIdentityConfiguration(
@@ -250,17 +250,17 @@ def launch_vm_and_job(  worker_id,
 
 
     # Delete the VM once the job is done  
-    #logging.info(f"Deleting compute instance {compute_instance_name}...")  
-    #delete_poller = ml_client.compute.begin_delete(compute_instance_name)
+    logging.info(f"Deleting compute instance {compute_instance_name}...")  
+    delete_poller = ml_client.compute.begin_delete(compute_instance_name)
     
     # Wait for resource cleanup  
-    #try:
-    #    logging.info("Waiting for instance deletion...")
-    #    delete_poller.result()
-    #    time.sleep(60)
-    #except Exception as err:
-    #    logging.error("Error while waiting for instance deletion...")
-    #    logging.exception(err)
+    try:
+        logging.info("Waiting for instance deletion...")
+        delete_poller.result()
+        time.sleep(60)
+    except Exception as err:
+        logging.error("Error while waiting for instance deletion...")
+        logging.exception(err)
     
 
 def launch_experiment(config):
